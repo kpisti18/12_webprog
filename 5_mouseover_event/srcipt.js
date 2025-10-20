fc6cdde3ececf31e5b581d35402fb3bd0541b3ab
kpisti18 <k.istvanzoltan@gmail.com>
2025-10-20 14:39:00 +0200

    mouseover event

---
+++ 5_mouseover_event/srcipt.js
@@ -0,0 +1,26 @@
+const demo = document.querySelector('#demo');
+//console.log(demo);
+let isImageDisplayed = false; // állapotjelző
+
+demo.addEventListener('mouseover', (event)=> {
+    //console.log(event);
+    if (!isImageDisplayed && event.target === demo) {
+        const img = document.createElement('img');
+        img.src = './img/kep.jpg';
+        img.alt = 'kep';
+
+        demo.replaceChildren(img);
+        isImageDisplayed = true;
+    }
+});
+
+demo.addEventListener('mouseout', (event) => {
+    // console.log(event);
+    if (isImageDisplayed && event.target === demo) {
+        const p = document.createElement('p');
+        p.textContent = 'Hanyas értékellek?';
+        
+        demo.replaceChildren(p);
+        isImageDisplayed = false;
+    }
+});
