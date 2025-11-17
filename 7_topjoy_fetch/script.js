3e023d801f3c3bb5bf33e89cda2af74373ef9cce
kpisti18 <k.istvanzoltan@gmail.com>
2025-11-17 14:11:24 +0100

    topjoy fetch

--- 7_topjoy_fetch/script.js
+++ 7_topjoy_fetch/script.js
@@ -6,10 +6,30 @@ const messageButton = document.querySelector('#messageButton');
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         const uzik = await fetch('./uzik.csv');
-        console.log(uzik);
+        //console.log(uzik);
+        uzikText = (await uzik.text())
+                            .split('\n')
+                            .map(sor => sor.trim());
+        //console.log(uzikText);
         
+        uzikText.forEach(uzi => {
+            messages.unshift(uzi)
+        });
+
     } catch (error) {
-        console.error(error);
+        console.log(error);
     }
 });
 
+//console.log(messages);
+
+// hozz létre a klikk eseményre, hogy véletlen üzenet jelenítsünk meg
+messageButton.addEventListener('click', () => {
+    if (messages.length > 0) {
+        const randomIndex = Math.floor(Math.random() * messages.length);
+
+        messageBox.textContent = messages[randomIndex];
+    } else {
+        messageBox.textContent = 'Nem található üzenet!';
+    }
+});
