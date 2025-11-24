913903f92e91f47ac12069a0d72616d8867516e9
kpisti18 <k.istvanzoltan@gmail.com>
2025-11-24 12:54:00 +0100

    topyjoy kepek forgatasaval

---
+++ 8_topjoy_kepek_forgatasaval/script.js
@@ -0,0 +1,29 @@
+const kupak = document.querySelector('#kupak');
+const szoveg = document.querySelector('#szoveg');
+const tarolo = document.querySelector('.tarolo');
+
+//console.log(kupak, szoveg, tarolo);
+const uzik = [];
+let zarvaVanE = true;
+
+// uzik.csv fetch
+window.addEventListener('DOMContentLoaded', async() => {
+    try {
+        const response = await fetch('./uzik.csv');
+        //console.log(response);
+        const text = await response.text();
+        //console.log(text);
+
+        const sorok = text.split('\n').map(sor => sor.trim());
+        //console.log(sorok);
+        
+        sorok.forEach(sor => {
+            uzik.push(sor);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }  
+});
+
+//console.log(uzik);
