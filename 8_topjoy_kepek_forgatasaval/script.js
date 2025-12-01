b30e91e912a6ab183d74d848d66e557558e9507d
kpisti18 <k.istvanzoltan@gmail.com>
2025-12-01 14:29:26 +0100

    8 topjoy done

--- 8_topjoy_kepek_forgatasaval/script.js
+++ 8_topjoy_kepek_forgatasaval/script.js
@@ -27,3 +27,31 @@ window.addEventListener('DOMContentLoaded', async() => {
 });
 
 //console.log(uzik);
+
+// Egy aszinkron, ami végiglépked a képeken.
+const animateCap = async (start, end) => {
+    const step = start < end ? 1 : -1; // Eldönti, hogy előre (kinyit) vagy visszafelé (bezár) kell lépkedni.
+
+    // Addig megy, amíg el nem éri a cél képek. Pl. 0->10 kinyitás, vagy 10->0 visszazárás
+    for (let index = start; index !== end + step; index+=step) {
+        console.log(index);
+        
+        // Vár 13ms-ot mielőtt kicserélné a képet, ez sima animációt eredményez.
+        await new Promise(resolve => setTimeout(resolve, 13));
+        kupak.src = `./images/bottlecap_${index}.png`;
+    }
+}
+
+tarolo.addEventListener('click', async () => {
+    if(zarvaVanE) {
+        //animálás kinyitáskor
+        await animateCap(0, 10);
+        szoveg.textContent = uzik[Math.floor(Math.random() * uzik.length)]
+        zarvaVanE = false;
+    } else {
+        //animálás záráskor
+        szoveg.textContent = '';
+        await animateCap(10, 0);
+        zarvaVanE = true;
+    }
+});
